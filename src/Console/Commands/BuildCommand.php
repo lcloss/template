b44a65517b5a417544e025e79efa7f22bc264c11
lcloss <lcloss@gmail.com>
2024-08-17 01:33:26 +0100

    Improve conversion

--- src/Console/Commands/BuildCommand.php
+++ src/Console/Commands/BuildCommand.php
@@ -56,15 +56,19 @@ class BuildCommand extends Command
         }
 
         // Caminho de origem e destino para a cópia dos arquivos
-        $sourcePath = resource_path('views/template/src/assets');
-        $destinationPath = public_path('assets');
 
         // Copiar a pasta /resources/views/templates/src/assets para /public/assets
-        if (File::exists($sourcePath)) {
-            File::copyDirectory($sourcePath, $destinationPath);
-            $this->info('A pasta assets foi copiada com sucesso para public/assets!');
-        } else {
-            $this->error('A pasta source /resources/views/template/src/assets não existe.');
+        $asset_folders = ['assets', 'css', 'js', 'img', 'vendor', 'scss'];
+        foreach($asset_folders as $asset_folder) {
+            $sourcePath = resource_path('views/template/src/' . $asset_folder);
+            $destinationPath = public_path($asset_folder);
+
+            if (File::exists($sourcePath)) {
+                File::copyDirectory($sourcePath, $destinationPath);
+                $this->info('A pasta ' . $asset_folder . ' foi copiada com sucesso para public/assets!');
+            } else {
+                $this->error('A pasta source /resources/views/template/src/' . $asset_folder . ' não existe.');
+            }
         }
 
         $this->info('Template built successfully!');
@@ -72,20 +76,43 @@ class BuildCommand extends Command
 
     private function convertHtmlToBlade(string $source): string
     {
+        // Create language keys for all content tags
+        $source = preg_replace('/placeholder="(.+?)"/', 'placeholder="{{ __(\'$1\') }}"', $source);
+
+        $content_tags = ['a', 'button', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'span', 'div', 'p'];
+        foreach($content_tags as $tag) {
+            preg_match_all('/<' . $tag . '[^>]*>([\w\s\-·,!]+?)<\/' . $tag . '>/', $source, $matches);
+            foreach( $matches[0] as $i => $match ) {
+                // Remove spaces, new lines and tabs
+                $literal = $matches[1][$i];
+                $literal = str_replace("\n", '', $literal);
+                $literal = str_replace("\t", '', $literal);
+                $literal = trim($literal);
+
+                // Replace all ocurrences
+                $replacement = str_replace($matches[1][$i], '{{ __(\'' . $literal . '\') }}', $match);
+                $source = str_replace($match, $replacement, $source);
+            }
+        }
+
         // Replaces <a href="X.html"> to <a href="{{ route('template', ['page' => 'X']) }}">
         $source = preg_replace('/href="(.+?)\.html"/', 'href="{{ route(\'template.page\', [\'page\' => \'$1\']) }}"', $source);
 
-        // Replaces src="PATHTO/X.jpg" to src="{{ asset('PATHTO/X.jpg') }}"
-        $source = preg_replace('/src="(.+?)\.jpg"/', 'src="{{ asset(\'$1.jpg\') }}"', $source);
-
-        // Replaces src="PATHTO/X.png" to src="{{ asset('PATHTO/X.png') }}"
-        $source = preg_replace('/src="(.+?)\.png"/', 'src="{{ asset(\'$1.png\') }}"', $source);
+        // Replaces src="PATHTO/X" to src="{{ asset('PATHTO/X') }}"
+        $source = preg_replace('/src="(.+?)"/', 'src="{{ asset(\'$1\') }}"', $source);
 
         // Replaces href="PATHTO/X.css" to href="{{ asset('PATHTO/X.css') }}"
         $source = preg_replace('/href="(.+?)\.css"/', 'href="{{ asset(\'$1.css\') }}"', $source);
 
         // Replaces src="PATHTO/X.js" to src="{{ asset('PATHTO/X.js') }}"
         $source = preg_replace('/src="(.+?)\.js"/', 'src="{{ asset(\'$1.js\') }}"', $source);
+
+        // Replaces url(PATHTO/X) to url({{ asset('PATHTO/X) }}"
+        $source = preg_replace('/url\((.+?)\)/', 'url({{ asset(\'$1\') }})', $source);
+
+        // Fix some incorrect blade syntax
+        $source = str_replace('{{ asset(\'.\') }}', '#', $source);
+        $source = str_replace('{{ asset(\'/\') }}', '{{ route(\'template.page\', [\'page\' => \'index\']) }}', $source);
 
         return $source;
     }
